b25ea13eb6c16c9daa2a300476f20521d3bf75f0
Yug Joshi <yugjoshi075@gmail.com>
2026-02-28 00:01:18 +0530

    Update userRoutes.js and index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 // server/index.js
+require("dotenv").config();
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
@@ -29,12 +30,12 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.get("/api/protected", authMiddleware, (req,res) => {
-    res.json({msg: "Protected route working ✅", user: req.user});
+app.get("/api/protected", authMiddleware, (req, res) => {
+    res.json({ msg: "Protected route working ✅", user: req.user });
 })
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/userController");
 
 // Define routes and connect them to controller functions
-router.post("/create", registerUser);
+router.post("/create", (req, res) => {
+    console.log("CREATE USER API HIT");
+    res.status(201).json({ msg: "User route working" });
+});
+
 router.post("/login", loginUser);
 
 module.exports = router;
